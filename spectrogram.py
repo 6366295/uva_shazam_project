f7fa86a882fcb5bada8f1869d973452bab572def
Mike <mt_tr1pl@hotmail.com>
2014-12-04 14:19:09 +0100

    ...

--- spectrogram.py
+++ spectrogram.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import scipy.io.wavfile as wavfile
 
-samplerate, data = wavfile.read('wav\alcedo_atthis_ijsvogel.wav')
+samplerate, data = wavfile.read('wav\\alcedo_atthis_ijsvogel.wav')
 
 print samplerate
+print data
 
 NFFT = 4096
 
-Pxx, freqs, bins, im = plt.specgram(data, NFFT=NFFT, Fs=samplerate, noverlap=int(4096*0.5), cmap=plt.cm.gist_rainbow)
+Pxx, freqs, bins, im = plt.specgram(data, NFFT=NFFT, Fs=samplerate, noverlap=int(4096*0.5), cmap=plt.cm.hot)
 plt.show()
 
 spectrum, freqs, t = mlab.specgram(data, NFFT=NFFT, Fs=samplerate, noverlap=int(4096*0.5))
